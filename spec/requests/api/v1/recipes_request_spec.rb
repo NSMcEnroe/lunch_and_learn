7ce90dbac97272b423be5e194c59305dd505fba2
Nicholas McEnroe <nsmcenro@gmail.com>
2023-11-12 12:05:00 -0700

    First commit

---
+++ spec/requests/api/v1/recipes_request_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "Search for Recipes", type: :request do
+  it "should return a list of recipes related to the keyword", :vcr do
+    user_search = "thailand"
+
+    get "/api/v1/recipes?country=#{user_search}"
+
+    expect(response).to be_successful
+
+    recipes_data = JSON.parse(response.body, symbolize_names: true)
+
+    recipes = recipes_data[:data]
+
+    recipes.each do |recipe|
+      expect(recipe).to have_key(:id)
+      expect(recipe[:id]).to eq(null)
+
+      expect(recipe).to have_key(:type)
+      expect(recipe[:id]).to eq("recipe")
+
+      expect(recipe).to have_key(:attributes)
+      expect(recipe[:attributes][:title]).to be_a(String)
+      expect(recipe[:attributes][:url]).to be_a(String)
+      expect(recipe[:attributes][:country]).to be_a(String)
+      expect(recipe[:attributes][:image]).to be_a(String)
+    end
+  end
