3246c3c9cf9875a776bcbc1feb2987c86bf38051
Nicholas McEnroe <nsmcenro@gmail.com>
2023-11-14 09:35:18 -0700

    Feat: Add sad path for US 2

--- app/poros/learning_resource.rb
+++ app/poros/learning_resource.rb
@@ -3,21 +3,29 @@ class LearningResource
   def initialize(video_data, picture_data, country)
     @country = country
     @video = parse_video_info(video_data)
-    @images = parse_photos(picture_data)
+    @images = parse_photos(picture_data) 
   end
 
   def parse_photos(picture_data)
-    picture_data.map do |photo|
-      { alt_tag: photo[:alt],
-        url: photo[:url]
-      }
+    if picture_data.nil?
+      []
+    else
+      picture_data.map do |photo|
+        { alt_tag: photo[:alt],
+          url: photo[:url]
+        }
+      end
     end
   end
 
   def parse_video_info(video_data)
-    {
-      title: video_data[:snippet][:title],
-      youtube_video_id: video_data[:id][:videoId]
-    }
+    if video_data.nil?
+      {}
+    else
+      {
+        title: video_data[:snippet][:title],
+        youtube_video_id: video_data[:id][:videoId]
+      }
+    end
   end
 end
--- spec/requests/api/v1/learning_resources_request_spec.rb
+++ spec/requests/api/v1/learning_resources_request_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Get Learning Resources for a Country", type: :request do
+  
   it "should return learning resources for Thailand", :vcr do
     user_search = "laos"
 
@@ -33,10 +34,35 @@ RSpec.describe "Get Learning Resources for a Country", type: :request do
       expect(image[:url]).to be_a(String)
     end
 
+    expect(json_data[:attributes]).to_not have_key :photographer
+    expect(json_data[:attributes]).to_not have_key :avg_color
+    expect(json_data[:attributes]).to_not have_key :thumbnail   
+    end
 
-      # expect(recipe[:attributes]).to_not have_key :yield
-      # expect(recipe[:attributes]).to_not have_key :ingredients
-      # expect(recipe[:attributes]).to_not have_key :images
+    it "should return the correct response if the video or images don't exist", :vcr do
+      user_search = "asdudgaskfgnasmtnreo"
+  
+      get "/api/v1/learning_resources?country=#{user_search}"
+  
+      expect(response).to be_successful
+  
+      learning_resources_data = JSON.parse(response.body, symbolize_names: true)
+  
+      json_data = learning_resources_data[:data]
+  
+      expect(json_data).to have_key(:id)
+      expect(json_data[:id]).to eq(nil)
+  
+      expect(json_data).to have_key(:type)
+      expect(json_data[:type]).to eq("learning_resource")
+  
+      expect(json_data).to have_key(:attributes)
+      expect(json_data[:attributes][:country]).to eq("asdudgaskfgnasmtnreo")
       
+      expect(json_data[:attributes]).to have_key(:video)
+      expect(json_data[:attributes][:video]).to eq({})
+
+      expect(json_data[:attributes]).to have_key(:images)
+      expect(json_data[:attributes][:images]).to eq([])
     end
   end
